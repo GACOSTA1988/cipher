d7df6833c436fd4a412f6e0899e21fc976fdc3f7
Student <example@example.com>
2019-10-21 15:38:16 -0700

    adds full functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 var sentencePrompt = prompt("Enter a sentence.");
 // console.log(sentencePrompt);
 var reversedString;
+var upperCaseString;
 console.log("Pre function: " + reversedString)
-var reversedCapitals = function(){
+var capitalizedFirstAndLast = function(){
   var newString = sentencePrompt[0] + sentencePrompt.slice(-1);
   // console.log("First Letter " + sentencePrompt[0]);
   // console.log("Last Letter: " + sentencePrompt.slice(-1));
-  console.log("Before : " + newString);
-  var upperCaseString = newString.toUpperCase()
-  console.log("After : " + upperCaseString);
-  reversedString = upperCaseString.split("").reverse().join("");
-  console.log("Reversed: " + reversedString)
-  // var typeOf = typeof(reversedString);
-  // console.log(typeOf);
+  console.log("Before Capilization: " + newString);
+  upperCaseString = newString.toUpperCase();
+  console.log("After Capilization: " + upperCaseString);
 }
-reversedCapitals();
+
+var  reversedFirstAndLast = function(){
+  reversedString = upperCaseString.split("").reverse().join("");
+  console.log("Reversed: " + reversedString);
+}
+
+var fullString = function(){
+  capitalizedFirstAndLast();
+  reversedFirstAndLast();
+  console.log(sentencePrompt + reversedString);
+};
+fullString();
