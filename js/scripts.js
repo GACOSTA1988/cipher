b682edcf120596c990e3720c8655a5961baa7251
Student <example@example.com>
2019-10-21 16:11:05 -0700

    adds functionality for all cipher instructions

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var sentencePrompt = prompt("Enter a sentence.");
 // console.log(sentencePrompt);
 var reversedString;
 var upperCaseString;
+var fullCipher;
 console.log("Pre function: " + reversedString)
 var capitalizedFirstAndLast = function(){
   var newString = sentencePrompt[0] + sentencePrompt.slice(-1);
@@ -12,7 +13,7 @@ var capitalizedFirstAndLast = function(){
   console.log("After Capilization: " + upperCaseString);
 }
 
-var  reversedFirstAndLast = function(){
+var reversedFirstAndLast = function(){
   reversedString = upperCaseString.split("").reverse().join("");
   console.log("Reversed: " + reversedString);
 }
@@ -20,6 +21,22 @@ var  reversedFirstAndLast = function(){
 var fullString = function(){
   capitalizedFirstAndLast();
   reversedFirstAndLast();
-  console.log(sentencePrompt + reversedString);
+  fullCipher = (sentencePrompt + reversedString);
+  console.log(fullCipher);
 };
 fullString();
+
+
+var fourthFunction = function(){
+//Count the number of letters in sentencePrompt
+  var sentenceLength = sentencePrompt.length;
+  console.log("Sentence Length: " + sentencePrompt.length);
+//Divide by two (may need to round down)
+  var sentenceHalfNumber = Math.round(sentenceLength / 2);
+  console.log("Sentence half: " + sentenceHalfNumber);
+//Output the letter at this index
+  splitPrompt = sentencePrompt.split("");
+//Concatenate this letter at the beginning of the original sentencePrompt
+  console.log((splitPrompt[sentenceHalfNumber]) + fullCipher);
+}
+fourthFunction();
