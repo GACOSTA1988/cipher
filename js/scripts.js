c623e6249ca4523e4664f0db88d0936a7e16b4ef
Student <example@example.com>
2019-10-21 15:18:05 -0700

    add functionality to split, reverse, and join cypher string

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 var sentencePrompt = prompt("Enter a sentence.");
-console.log(sentencePrompt);
+// console.log(sentencePrompt);
+var reversedString;
+console.log("Pre function: " + reversedString)
 var reversedCapitals = function(){
-  console.log("First Letter " + sentencePrompt[0]);
-  console.log("Last Letter: " + sentencePrompt.slice(-1));
+  var newString = sentencePrompt[0] + sentencePrompt.slice(-1);
+  // console.log("First Letter " + sentencePrompt[0]);
+  // console.log("Last Letter: " + sentencePrompt.slice(-1));
+  console.log("Before : " + newString);
+  var upperCaseString = newString.toUpperCase()
+  console.log("After : " + upperCaseString);
+  reversedString = upperCaseString.split("").reverse().join("");
+  console.log("Reversed: " + reversedString)
+  // var typeOf = typeof(reversedString);
+  // console.log(typeOf);
 }
 reversedCapitals();
